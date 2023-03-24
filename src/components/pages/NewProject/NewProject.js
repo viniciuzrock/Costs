9c151d779032bbde722f0d7b3fac67bda620803e
viniciuzrock <vinir.santoss@gmail.com>
2023-03-23 23:29:32 -0300

    Ajustando props nos input components

--- src/components/pages/NewProject/NewProject.js
+++ src/components/pages/NewProject/NewProject.js
@@ -1,14 +1,36 @@
+import { useHistory } from "react-router-dom"
 import ProjectForm from "../../project/ProjectForm"
 import styles from "./NewProject.module.css"
 
 function NewProject(){
+
+    // const history = useHistory()
+
+    function createPost(project){
+        project.cost = 0;
+        project.services = [];
+
+        fetch("http://localhost:5000/projects",{
+            method:"POST",
+            headers:{
+                "Content-type":"application-json"
+            },
+            body: JSON.stringify(project)
+        }).then((resp)=> resp.json()).then((data)=>{
+            console.log(data)
+
+        }).catch((e)=>{
+            console.log(e)
+        })
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicioanr os serviços</p>
-            <ProjectForm btnText="Criar projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projeto"/>
         </div>
     )
 }
 
-export default NewProject
+export default NewProject;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,44 +1,77 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+// import { useEffect } from "react"
 import Input from "../form/Input/Input"
 import Select from "../form/Select/Select"
 import SubmitButton from "../form/SubmitButton/SubmitButton"
 import styles from "./ProjectForm.module.css"
 
-function ProjectForm({btnText}){
+function ProjectForm({btnText, handleSubmit, projectData}){
 
-    const [categories, setCategories] = useState([])
+    const [categories, setCategories] = useState([]);
+    const [project, setProject] = useState([ projectData || {} ]);
 
-    fetch("http://localhost:5000/categories",{
-        method:"GET",
-        headers:{
-            "Content-type":"application/json"
-        }
-    }).then((resp)=> resp.json()).then((data)=>{
-        setCategories(data)
-    }).catch((e)=>{
-        console.log(e)
-    })
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories",{
+            method:"GET",
+            headers:{
+                "Content-type":"application/json"
+            }
+        }).then((resp)=> resp.json()).then((data)=>{
+            setCategories(data)
+        }).catch((e)=>{
+            console.log(e)
+        })
+    }, []);
 
+    //Na hora do submit do form, irá disparar o Post no elemento pai
+    const submit = (e)=>{
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e){
+        //1                2            3               4
+        setProject({ ...project, [e.target.name]: e.target.value});
+        console.log(project);
+    }
+    /*
+    * 1 - Novo valor de 'project'
+    * 2 - project 'no momento'
+    * 3 - nome do campo que foi digitado
+    * 4 - valor digitado
+    */
+
+    function handleCategory(e){
+        setProject({
+            ...project,
+            category:{
+                id: e.target.value,
+                name: e.target.options[e.target.selectedIndex].text
+            }
+        })
+    }
     return(
 
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <Input
                 type="text"
                 text="Nome do projeto"
                 name="name"
                 placeholder="Insira o nome do projeto"
+                handleOnChange={handleChange}
             />
             <Input
                 type="number"
                 text="Orçamento do projeto"
                 name="budget"
                 placeholder="Insira o valor do projeto"
+                handleOnChange={handleChange}
             />
             <Select
                 name="category_id"
                 text="Selecione a categoria"
                 options={categories}
+                handleOnChange = {handleCategory}
             />
             <SubmitButton text={btnText}/>
         </form>
