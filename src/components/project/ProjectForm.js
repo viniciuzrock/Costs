ff0b0c99f6c6ef7338dc65ae0974e6939103092f
viniciuzrock <vinir.santoss@gmail.com>
2023-03-23 21:22:38 -0300

    Recebendo dados da API e arrumando select

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,11 +1,28 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import Input from "../form/Input/Input"
 import Select from "../form/Select/Select"
 import SubmitButton from "../form/SubmitButton/SubmitButton"
 import styles from "./ProjectForm.module.css"
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    fetch("http://localhost:5000/categories",{
+        method:"GET",
+        headers:{
+            "Content-type":"application/json"
+        }
+    }).then((resp)=> resp.json()).then((data)=>{
+        setCategories(data)
+    }).catch((e)=>{
+        console.log(e)
+    })
+
     return(
-        <form>
+
+        <form className={styles.form}>
             <Input
                 type="text"
                 text="Nome do projeto"
@@ -21,6 +38,7 @@ function ProjectForm({btnText}){
             <Select
                 name="category_id"
                 text="Selecione a categoria"
+                options={categories}
             />
             <SubmitButton text={btnText}/>
         </form>
